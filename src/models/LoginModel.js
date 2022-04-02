86b86385c43c093f67c507feb65dbe46d5f16528
IsaqueMansur <isaquemansur@gmail.com>
2022-04-02 20:56:37 -0300

    verificação de criação de usuário repetido

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 const LoginSchema = new mongoose.Schema({
     email: { type: String, required: true },
     password: { type: String, required: true }
@@ -10,22 +11,31 @@ class Login {
     constructor(body) {
         this.body = body;
         this.errors = [];
-        this.user = null;
-        
+        this.user = null;   
     }
 
     async register() {
         this.valida();
+
+        await this.userExists();
+
         if (this.errors.length > 0) return;
+
+        const salt = bcryptjs.genSaltSync();
+        this.body.password = bcryptjs.hashSync(this.body.password, salt);
+
         try {
-            const salt = bcryptjs.genSalsSync();
-            this.body.password = bcryptjs.hashSync(this.body.password, salt);
             this.user = await LoginModel.create(this.body);
         }
         catch(e) {
             console.log(e);
             return e;
         }
+    }
+
+    async userExists() {
+        const user = await LoginModel.findOne({email: this.body.email});
+        if (user) this.errors.push('Usuário já existe');
     }
 
     valida() {
