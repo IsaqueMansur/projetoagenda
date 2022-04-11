9a0cf8d6b212da56a72a84ec1205b0c21f74f6b5
IsaqueMansur <isaquemansur@gmail.com>
2022-04-10 21:53:06 -0300

    Edição de contatos pós criação - OK

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -30,8 +30,30 @@ exports.editIndex = async function (req, res) {
     if (!req.params.id) return res.render('404');
 
     const contato = await Contato.buscaPorId(req.params.id);
+    console.log(req.params.id);
 
     if (!contato) return res.render('404');
 
     res.render('contato', { contato });
 }
+
+exports.edit = async (req, res) => {
+    try {
+        if(!req.params.id) return res.render('404');
+        const contato = new Contato(req.body);
+        console.log(req.params.id);
+        await contato.edit(req.params.id);
+
+        if (contato.errors.length > 0) {
+            req.flash('errors', contato.errors);
+            req.session.save(() => res.redirect('back'));
+            return;
+        }
+        req.flash('success', 'Contato editado com sucesso');
+        req.session.save(() => res.redirect(`/contato/index/${contato.contato._id}`));
+        return;
+    } catch(e) {
+        console.log(e);
+        res.render('404');
+    }    
+};
